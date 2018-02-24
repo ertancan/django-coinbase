b7ff7381994bda13424b55b1d96852165b3b5fc4
ertan <ertan@profitwolf.eu>
2018-02-24 16:28:38 +0000

    fixing view import

--- coinbase/urls.py
+++ coinbase/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-
+from views import handle_order_callback
 
 urlpatterns = [
-    url(r"^cb/$", "handle_order_callback", name="coinbase_order_callback")
+    url(r"^cb/$", handle_order_callback, name="coinbase_order_callback")
     ]
